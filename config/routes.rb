0817f9c655312775d2294855118d608b083296f3
Vuducvi1999 <vuducvi20@gmail.com>
2021-10-30 20:03:41 +0700

    config cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
+
+# check_access!     require access_api or token
+# authorized!       require token
+
+
 Rails.application.routes.draw do
-  root 'home_page#home'
+  root 'home_page#home'                           # public
   
-  post 'sign-in' => 'authenticate#sign_in'
-  post 'sign-up' => 'authenticate#sign_up'
+  post 'sign-in' => 'authenticate#sign_in'        # authorized!
+  post 'sign-up' => 'authenticate#sign_up'        # authorized!
   
-  post 'compress' => 'shorten_url#compress'
+  post 'compress' => 'shorten_url#compress'       # check_access!
   
-  get 'access-api' => 'management#access_api'
-  get 'links' => 'management#links'
-  get 'detail/:alias' => 'management#detail'
-  post 'destroy/:alias' => 'management#destroy'
-  post 'edit/:alias' => 'management#edit'
+  get 'access-api' => 'management#access_api'     # authorized!
+  get 'links' => 'management#links'               # check_access!
+  get 'detail/:alias' => 'management#detail'      # check_access!
+  post 'destroy/:alias' => 'management#destroy'   # check_access!
+  post 'edit/:alias' => 'management#edit'         # check_access!
   
-  get ':alias' => 'shorten_url#direct'
+  get ':alias' => 'shorten_url#direct'            # public
 end
