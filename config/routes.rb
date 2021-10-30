44c006544024c2b6e544b0134f71e02595f9efce
Vuducvi1999 <vuducvi20@gmail.com>
2021-10-30 21:01:19 +0700

    default format json routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,20 @@
 
 
 Rails.application.routes.draw do
-  root 'home_page#home'                           # public
-  
-  post 'sign-in' => 'authenticate#sign_in'        # authorized!
-  post 'sign-up' => 'authenticate#sign_up'        # authorized!
-  
-  post 'compress' => 'shorten_url#compress'       # check_access!
-  
-  get 'access-api' => 'management#access_api'     # authorized!
-  get 'links' => 'management#links'               # check_access!
-  get 'detail/:alias' => 'management#detail'      # check_access!
-  post 'destroy/:alias' => 'management#destroy'   # check_access!
-  post 'edit/:alias' => 'management#edit'         # check_access!
-  
-  get ':alias' => 'shorten_url#direct'            # public
+  defaults format: :json do
+    root 'home_page#home'                           # public
+    
+    post 'sign-in' => 'authenticate#sign_in'        # authorized!
+    post 'sign-up' => 'authenticate#sign_up'        # authorized!
+    
+    post 'compress' => 'shorten_url#compress'       # check_access!
+    
+    get 'access-api' => 'management#access_api'     # authorized!
+    get 'links' => 'management#links'               # check_access!
+    get 'detail/:alias' => 'management#detail'      # check_access!
+    post 'destroy/:alias' => 'management#destroy'   # check_access!
+    post 'edit/:alias' => 'management#edit'         # check_access!
+    
+    get ':alias' => 'shorten_url#direct'            # public
+  end
 end
